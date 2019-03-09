3808612fcfeb679f152111a4c2c39d23c5ae7a5b
HaileyGu <47661750+HaileyGu@users.noreply.github.com>
2019-03-09 16:38:51 +0900

    maxCounters

--- Codility/004_04_MaxCounters.java
+++ Codility/004_04_MaxCounters.java
@@ -1,28 +1,41 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
 class Solution {
     public int[] solution(int N, int[] A) {
         // write your code in Java SE 8
+        int maxCounter = 0;
+        int tempCounter = 0;
+        int[] result = new int[N];
 
-      int[] counters = new int[N];
+        for(int i = 0; i < A.length; i++) {
+           // System.out.println(A[i]);
+            if(A[i] == N+1) {
+                maxCounter = tempCounter;
+            }
+            else {
+                if(maxCounter > result[A[i]-1])
+                    result[A[i]-1] = maxCounter;
 
-  		int currMax = 0;
-  		int currMin = 0;
+                result[A[i]-1]++;
 
-  		for (int i = 0; i < A.length; i++) {
+                if(result[A[i]-1] > tempCounter)
+                    tempCounter = result[A[i]-1];
 
-  			if (A[i] <= N) {
-  				counters[A[i]-1] = Math.max(currMin, counters[A[i]-1]);
-  				counters[A[i]-1]++;
-  				currMax = Math.max(currMax, counters[A[i]-1]);
-  			}
-  			else if (A[i] == N+1) {
-  				currMin= currMax;
-  			}
-  		}
+               // System.out.println(maxCounter);
+            }
 
-  		for (int i = 0; i < counters.length; i++) {
-  			counters[i] = Math.max(counters[i], currMin);
-  		}
+           // System.out.println("" + result[0]+ result[1]+ result[2]+ result[3]+ result[4]);
+        }
 
-  		return counters;
+        for(int j = 0; j < N; j++) {
+            if(maxCounter > result[j])
+                result[j] = maxCounter;
+        }
+
+        return result;
     }
 }
