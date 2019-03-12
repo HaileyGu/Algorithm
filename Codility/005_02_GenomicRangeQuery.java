2cb5b5b9b3dba7b8dc6a198019e08ed8ff3f8b99
HaileyGu <47661750+HaileyGu@users.noreply.github.com>
2019-03-12 18:55:12 +0900

    change map to int (100%)

--- Codility/005_02_GenomicRangeQuery.java
+++ Codility/005_02_GenomicRangeQuery.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(String S, int[] P, int[] Q) {
         int N = P.length;
-        Map<Integer, ArrayList<Integer>> prefSums = getPrefSum(S);
+        int[][] prefSums = new int[S.length()][4];
+        prefSums = getPrefSum(S, prefSums);
 
 		int[] ans = new int[N];
 		for (int i = 0; i < N; i++) {
-			for (int j = 1; j < 5; j++) {
-				int high = prefSums.get(j).get(Q[i]);
-				int low = P[i]==0? 0 :prefSums.get(j).get(P[i] -1);
+			for (int j = 0; j < 4; j++) {
+				int high = prefSums[Q[i]][j];
+				int low = P[i]==0? 0 :prefSums[P[i]-1][j];
 				if (high - low > 0) {
-					ans[i]=j;
+					ans[i]=j+1; // A,C,G,T
 					break;
 				}
 			}
@@ -19,27 +20,17 @@ class Solution {
 		return ans;
     }
 
-    public static Map<Integer,ArrayList<Integer>> getPrefSum(String s) {
+    public static int[][] getPrefSum(String s, int[][] prefSum) {
 
-		Map<Integer, ArrayList<Integer>> prefSums = new HashMap<Integer, ArrayList<Integer>>();
-
-		for (int j = 0; j < 4; j++) {
-			prefSums.put(j+1, new ArrayList<Integer>()); // 1,2,3,4
-		}
-
-		int[] counters = new int[4];
 		for (int i = 0; i < s.length(); i++) {
 			switch(s.charAt(i)) {
-				case 'A':   counters[0]++; break;
-				case 'C':   counters[1]++; break;
-				case 'G':   counters[2]++; break;
-				case 'T':   counters[3]++; break;
+				case 'A':   prefSum[i][0]++; break;
+				case 'C':   prefSum[i][1]++; break;
+				case 'G':   prefSum[i][2]++; break;
+				case 'T':   prefSum[i][3]++; break;
 				default:    break;
 			}
-			for (int j = 0; j < 4; j++) {
-				prefSums.get(j+1).add(counters[j]);
-			}
 		}
-		return prefSums;
+		return prefSum;
 	}
 }
