ecdaf46de45e291c52a67152f3af590e6dc61b3e
Hailey Gu <47661750+HaileyGu@users.noreply.github.com>
2021-11-04 18:21:45 +0100

    Create 0129_Sum_Root_to_Leaf_Numbers.py

---
+++ LeetCode/0129_Sum_Root_to_Leaf_Numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        root_to_leaf = 0
+        stack = [(root, 0)]
+        
+        while stack:
+            root, curr_number = stack.pop()
+            if root is not None:
+                curr_number = curr_number * 10 + root.val
+                if root.left is None and root.right is None:
+                    root_to_leaf += curr_number
+                else:
+                    stack.append((root.right, curr_number))
+                    stack.append((root.left, curr_number))
+                    
+        return root_to_leaf
